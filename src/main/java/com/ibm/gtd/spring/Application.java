628d85ae073571ba1642694eb700009e7296b4e6
Sushil Verma <sushil@Sushils-MacBook-Pro.local>
2018-01-23 11:51:56 +0530

    Third commit

--- src/main/java/com/ibm/gtd/spring/Application.java
+++ src/main/java/com/ibm/gtd/spring/Application.java
@@ -1,5 +1,19 @@
 package com.ibm.gtd.spring;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 }
